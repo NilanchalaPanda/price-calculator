95e5417e5bcf3c191d09aa70dfbe0ed4fb840d4a
Nilanchal <110488337+NilanchalaPanda@users.noreply.github.com>
2025-08-26 16:37:54 +0530

    feat: added common io interface

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
-
-	"example.com/price-cal/filemanager"
+	"example.com/price-cal/cmdmanager"
 	"example.com/price-cal/prices"
 )
+
+type IOManager interface {
+	ReadLines() ([]string, error)
+	WriteResult(data interface{}) error
+}
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
@@ -15,8 +18,11 @@ func main() {
 	// result := make(map[float64][]float64)
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+
+		cmdm := cmdmanager.New()
+
+		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
 }
